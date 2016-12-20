289f13ce415293c6176b2adb50dbaa212420c797
Lorna <lornanqodi@yahoo.com>
2016-12-20 10:30:44 +0200

    Added a basic layout for add a feature page on App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import logo from './AG-Logo.png';
 import './App.css';
+//import FormField from '@agct/react-common/src/components/FormField/FormField';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <div id="main_div">
-          <center>
-            <a className="primary_button feat-mng" href="feature_manage.html">Feature Management</a>
-            <br />
-            <a className="secondary_button group-mng" href="group_manage">Group Management</a>
-          </center>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <div id="main_div">
+        <div className="form-style-8">
+          <h1>Add a Feature</h1>
+          <form className="text-input">
+            <input type="text" name="field1" placeholder="Enter feature name" />
+            <input type="button" value="Save" />
+          </form>
         </div>
       </div>
-    );
+      </div>
+      );
   }
 }
 
